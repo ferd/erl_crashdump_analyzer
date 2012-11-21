abd2e6b11b309e65e2c821b8cdc99690cbbc6f7f
Fred Hebert <mononcqc@gmail.com>
2012-11-21 13:46:37 -0500

    Adding process states at the time of the crashdump

--- erl_crashdump_analyzer.sh
+++ erl_crashdump_analyzer.sh
@@ -36,30 +36,34 @@ M=`grep -m 1 'total' $DUMP | sed "s/total: //"`
 let "m=$M/(1024*1024)"
 echo -e "  ---\n  total: $m Mb"
 
-### PROCESS MESSAGE QUEUES LENGTHS
+### PROCESS MESSAGE QUEUES LENGTHS ###
 echo -e "\nDifferent message queue lengths (5 largest different):\n==="
 grep 'Message queue len' $DUMP | sed 's/Message queue length: //g' | sort -n -r | uniq -c | head -5 
 
-### ERROR LOGGER QUEUE LENGTH
+### ERROR LOGGER QUEUE LENGTH ###
 echo -e "\nError logger queue length:\n==="
 grep -C 10 'Name: error_logger' $DUMP -m 1| grep 'Message queue length' | sed 's/Message queue length: //g'
 
 
-### PORT/FILE DESCRIPTOR INFO
+### PORT/FILE DESCRIPTOR INFO ###
 echo -e "\nFile descriptors open:\n==="
 echo -e "  UDP: "   `grep 'Port controls linked-in driver:' $DUMP | grep 'udp_inet' | wc -l`
 echo -e "  TCP: "   `grep 'Port controls linked-in driver:' $DUMP | grep 'tcp_inet' | wc -l`
 echo -e "  Files: " `grep 'Port controls linked-in driver:' $DUMP | grep -vi 'udp_inet' | grep -vi 'tcp_inet' | wc -l`
 echo -e "  ---\n  Total: " `grep 'Port controls linked-in driver:' $DUMP | wc -l`
 
-### NUMBER OF PROCESSES
+### NUMBER OF PROCESSES ###
 echo -e "\nNumber of processes:\n==="
 grep '=proc:' $DUMP | wc -l
 
-### PROC HEAPS+STACK
+### PROC HEAPS+STACK ###
 echo -e "\nProcesses Heap+Stack memory sizes (words) used in the VM (5 largest different):\n==="
 grep 'Stack+heap' $DUMP | sed "s/Stack+heap: //g" | sort -n -r | uniq -c | head -5
 
-### PROC OLDHEAP
+### PROC OLDHEAP ###
 echo -e "\nProcesses OldHeap memory sizes (words) used in the VM (5 largest different):\n==="
 grep 'OldHeap' $DUMP | sed "s/OldHeap: //g" | sort -n -r | uniq -c | head -5
+
+### PROC STATES ###
+echo -e "\nProcess States when crashing (sum): \n==="
+grep 'State: ' $DUMP | sed "s/State: //g" | sort | uniq -c
